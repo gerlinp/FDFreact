1f8ac7859dac65fcf6c84d229934af2d5d86fe93
gerlinp <gerlin.pierrelouis@gmail.com>
2022-01-22 22:27:14 -0500

    getting back in the game

--- EcommerceAPI/index.js
+++ EcommerceAPI/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose")
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
 
 dotenv.config();
 
@@ -14,8 +16,13 @@ mongoose
 
     app.get("/api/test", ()=>{
         console.log("test is successfull")
-    })
+    });
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log("Backend server is running!")
+    app.use(express.json());
+    app.use("/api/auth",authRoute);
+    app.use("/api/users",userRoute);
+    
+
+    app.listen(process.env.PORT || 5000, () => {
+        console.log("Backend server is running!")
 });
